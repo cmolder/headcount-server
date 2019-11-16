126c160b81851c1036dbe04d1556a3620f60ce18
City303 <City303@users.noreply.github.com>
2019-11-16 17:57:47 -0600

    Configure for Heroku

--- headcount_api/headcount_api/settings.py
+++ headcount_api/headcount_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,3 +144,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 #     'http://localhost:19002'       # Port for Expo CLI app on computer
 # )
 
+django_heroku.settings(locals())
