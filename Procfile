7137c97dac254c9c1e9bf0c343af8f021c6ed546
City303 <City303@users.noreply.github.com>
2019-11-16 18:26:20 -0600

    More heroku work

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn headcount_api.wsgi --log-file -
--- headcount_api/headcount_api/settings.py
+++ headcount_api/headcount_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z96qavo-q#^p1lmbna6y1xo7ll!dyj#=bjy&o!ygibbolu^u%b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['headcount-server.herokuapp.com']
 
 
 # Application definition
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     # Third party
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
@@ -133,8 +134,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = (
@@ -145,3 +155,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # )
 
 django_heroku.settings(locals())
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
