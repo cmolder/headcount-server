9888b6a3e862e87b7bdfcdc7896fb2dc7357625a
City303 <City303@users.noreply.github.com>
2019-11-19 12:58:05 -0600

    Changed URL structure slightly for API

--- headcount_db/urls.py
+++ headcount_db/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('classrooms', views.ListClassroom.as_view()),            # Gets all classrooms in the database
-    path('classrooms/<int:pk>', views.DetailClassroom.as_view()), # Gets classroom w/ provided Django ID
+    path('classroom', views.ListClassroom.as_view()),            # Gets all classrooms in the database
+    path('classroom/<int:pk>', views.DetailClassroom.as_view()), # Gets classroom w/ provided Django ID
     
-    path('students', views.ListStudent.as_view()),            # Gets all students in the database
-    path('students/<int:pk>', views.DetailStudent.as_view()), # Gets student w/ provided Django ID
+    path('student', views.ListStudent.as_view()),            # Gets all students in the database
+    path('student/<int:pk>', views.DetailStudent.as_view()), # Gets student w/ provided Django ID
 ]
