0a0abfe6fc1a196167732d142f1407329e5c830b
City303 <City303@users.noreply.github.com>
2019-11-22 12:15:02 -0600

    AttendanceTransaction validation now considers if Student is on Classroom student roster

--- headcount_db/models.py
+++ headcount_db/models.py
@@ -19,14 +19,7 @@ class Classroom(models.Model):
     active     = models.BooleanField(default=False) # Class code is generated when class is active
 
 
-    ''' String representation '''
-    def __str__(self):
-        if self.class_code is not None:
-            return (self.department + " " + self.number + " " + self.name + " " + self.class_code)
-        return (self.department + " " + self.number + " " + self.name + " (inactive)")
-
-
-    ''' Updates the class code upon being called (if it is necessary to do so) '''
+    ''' Updates the class code upon being activated '''
     def update_class_code(self):
         if(self.active == False):
             self.class_code = None
@@ -35,10 +28,17 @@ class Classroom(models.Model):
             self.class_code = get_random_string(6).upper()
 
 
-    ''' Actions to perform upon saving '''
+    ''' Saves the Classroom '''
     def save(self, *args, **kwargs):
         self.update_class_code()
         return super(Classroom, self).save(*args, **kwargs)
+
+    
+    ''' String representation '''
+    def __str__(self):
+        if self.class_code is not None:
+            return (self.department + " " + self.number + " " + self.name + " " + self.class_code)
+        return (self.department + " " + self.number + " " + self.name + " (inactive)")
 
 
 
@@ -76,17 +76,15 @@ Attendance transaction model
 class AttendanceTransaction(models.Model):
 
     
-    ''' Classroom validator '''
+    ''' Validates the Classroom '''
     def validate_classroom(classroom):
-        '''
-        Django Admin interface sends an integer representing classroom's
-        Django ID, so we need to parse it
-        ------
-        Django Rest API sends the actual Classroom object, so we can
-        directly parse the field
-        '''
+        # Django Admin interface sends an integer representing a Classroom's
+        # Django ID, so we need to obtain the associated Classroom first
+        # ------
+        # Django REST API sends the actual Classroom object, so we can
+        # directly parse the field
         if type(classroom) is int:
-            classroom = Classroom.objects.get(id = classroom) # Gets Classroom from provided ID
+            classroom = Classroom.objects.get(id = classroom)
 
         if type(classroom) is Classroom:
             if classroom.active is not True:
@@ -96,12 +94,22 @@ class AttendanceTransaction(models.Model):
 
 
     ''' Attendance transaction fields '''
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)     # Student associated with attendance request
-    classroom = models.ForeignKey('Classroom', validators=[validate_classroom], on_delete=models.CASCADE) # Classroom associated with attendance request (empty if inactive)
-    time = models.DateTimeField(editable=False)                          # Time of attendance request
+    classroom = models.ForeignKey('Classroom', validators=[validate_classroom], on_delete=models.CASCADE)
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    time = models.DateTimeField(editable=False)
         
 
-    ''' Actions to perform upon saving '''
+    ''' Validates the attendance transaction as a whole  '''
+    def clean(self):
+        # If the student is not on the class roster, then the
+        # attendance transaction is not valid
+        if self.student not in self.classroom.students.all():
+            raise ValidationError((f"{self.student} is not on the roster for {self.classroom}"), 
+                                  params={'student': self.student, 'classroom': self.classroom})
+
+
+
+    ''' Saves the AttendanceTransaction '''
     def save(self, *args, **kwargs):
         # On creation, set the time field to current time
         if not self.id:
