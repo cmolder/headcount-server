6aae0f3ff2ffced7187c7c02a30c142944cd4552
City303 <City303@users.noreply.github.com>
2019-11-23 16:11:31 -0600

    Added class_code and student_id queries to AttendanceTransaction

--- headcount_db/views.py
+++ headcount_db/views.py
@@ -14,10 +14,10 @@ class ListClassroom(generics.ListCreateAPIView):
         with the given six-digit class code
         '''
         queryset = Classroom.objects.all()
-        code = self.request.query_params.get('class_code', None)
+        class_code = self.request.query_params.get('class_code', None)
 
-        if code is not None:
-            queryset = queryset.filter(class_code = code)
+        if class_code is not None:
+            queryset = queryset.filter(class_code = class_code)
 
         return queryset
 
@@ -33,7 +33,7 @@ class ListStudent(generics.ListCreateAPIView):
 
     def get_queryset(self):
         '''
-        class_code - (optional) restricts the returned Students to the one
+        student_id - (optional) restricts the returned Students to the one
         with the given nine-digit student ID
         '''
         queryset = Student.objects.all()
@@ -60,23 +60,42 @@ class ListAttendanceTransaction(generics.ListCreateAPIView):
         attendance transactions to those associated with the Classroom
         with the given Django ID. [ NOT classroom's class_code!! ]
 
+        class_code - (optional) restricts the returned
+        attendance transactions to those associated with the active Classroom
+        that currently has the given class code.
+
         student - (optional) restricts the returned
         attendance transactions to those associated with the Student
         with the given Django ID. [ NOT student's student_id!! ]
 
-        date - (optional) in form YEAR-MM-DD restricts the returned
+        student_id - (optional) restricts the returned
+        aatendance transactions to those associated with the Student
+        with the given nine-digit student id.
+
+        date - (optional) in form YYYY-MM-DD restricts the returned
         attendance transactions to those that happened on said date.
         '''
-        queryset  = AttendanceTransaction.objects.all()
-        student   = self.request.query_params.get('student', None)
-        classroom = self.request.query_params.get('classroom', None)
-        date      = self.request.query_params.get('date', None)
+        queryset   = AttendanceTransaction.objects.all()
+
+        classroom  = self.request.query_params.get('classroom', None)
+        class_code = self.request.query_params.get('class_code', None)
+
+        student    = self.request.query_params.get('student', None)
+        student_id = self.request.query_params.get('student_id', None)
+
+        date       = self.request.query_params.get('date', None)
 
         if classroom is not None:
             queryset = queryset.filter(classroom__id = classroom)
 
+        if class_code is not None:
+            queryset = queryset.filter(classroom__class_code = class_code)
+
         if student is not None:
             queryset = queryset.filter(student__id = student)
+
+        if student_id is not None:
+            queryset = queryset.filter(student__student_id = student_id)
 
         if date is not None:
             queryset = queryset.filter(time__date = date)
